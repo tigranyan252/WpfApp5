5e102672695b422d6b6b30b83f7c9f0745c065e7
Serj Tigranyan <serj-tigranyan@mail.ru>
2022-03-11 19:29:45 +0200

    Добавьте файлы проекта.

---
+++ WpfApp5/Example.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    internal class Example
+    {
+        private int x;
+        private int y;
+        public Example(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int Output()
+        {
+            return (int)Math.Pow(Math.Max(x, y), 3);
+        }
+    }
+}
